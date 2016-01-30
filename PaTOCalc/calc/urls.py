cfe4117ea49a7b80d0677e86d7ad175483303eb9
Martin Green <martin.speleo@gmail.com>
2016-01-30 16:35:06 +0000

    start of rendering forms

--- PaTOCalc/calc/urls.py
+++ PaTOCalc/calc/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # Admin home page
     url(r'^admin_home_page/', admin_home_page, name ="admin_home_page"),
     url(r'^submit_new_form/', submit_new_form, name ="submit_new_form"),
+    url(r'^new_form_instance/([^/]*)/([^/]*)', new_form_instance, name ="new_form_instance"),
     
 
 ]
--- PaTOCalc/calc/views.py
+++ PaTOCalc/calc/views.py
@@ -5,9 +5,10 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
 from django.template import RequestContext
+from django.shortcuts import get_object_or_404
 
 from calc.forms import AddFormGenerator
-from calc.models import FormGenerator
+from calc.models import FormGenerator, FormInstance
 
 @login_required
 def admin_home_page(request):
@@ -18,3 +19,10 @@ def admin_home_page(request):
 def submit_new_form(request):
     ctx = {'user' : request.user, 'form' : AddFormGenerator }
     return render_to_response('calc/submit_form.html', ctx)
+
+@login_required
+def new_form_instance(request, fi_pk, mrn):
+    fi = get_object_or_404(FormInstance, pk = fi_pk)
+    #patient = get_patient ...
+    ctx = {'user' : request.user, 'form': fi}
+    return render_to_response('calc/form_instance.html', ctx)
