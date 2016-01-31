6f36de3dc3919e4093780cc8a77b8026c3521c3c
Mike Smith <mike.smith@camc-ltd.co.uk>
2016-01-31 07:23:46 +0000

    + API Patient search with OpenEyes (WIP)

--- PaTOCalc/patient/functions.py
+++ PaTOCalc/patient/functions.py
@@ -1,4 +1,7 @@
 from django.conf import settings
+import requests
+import base64
+
 
 def get_current_patient(request):
     return request.session.get('current_patient', None)
@@ -13,8 +16,26 @@ def search_for_patient(search_term):
     if not hasattr(settings, 'PATIENT_SOURCE'):
         return {'id': search_term}
     elif settings.PATIENT_SOURCE == 'openeyes':
-        print("here")
-        # do OE search and return patient results
-        return {'id': 'OE - ' + str(search_term)}
+        url = settings.OPENEYES_URL + '/api/Patient?identifier=' + str(search_term)
+        auth = "{" + settings.OPENEYES_USER + ":" + settings.OPENEYES_PASSWORD + "}"
+        headers = {
+            'Basic': base64.b64encode(bytes(auth, 'utf-8')),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        response = requests.get(url, headers=headers).json()
+
+        if (hasattr(response, 'entry')):
+            entries = response.entry
+            if (len(entries) > 1):
+                return {'id': 'OE - Multiple results not implemented yet for search ' + str(search_term)}
+            elif (len(entries) == 1):
+                patient = entries[0].content
+                return {
+                    'id': patient.name[0].given + ' ' + patient.name[0].family
+                }
+            else:
+                return None
     else:
         raise Exception('unregonised patient search configuration')
