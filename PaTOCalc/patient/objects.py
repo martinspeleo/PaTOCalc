025007dc131c69f44c3983e59465059fb0c45994
Mike Smith <mike.smith@camc-ltd.co.uk>
2016-01-31 12:11:23 +0000

    default values for base Patient object

--- PaTOCalc/patient/objects.py
+++ PaTOCalc/patient/objects.py
@@ -4,22 +4,39 @@ import json
 class Patient(json.JSONEncoder):
     hos_num = None
 
+    obs = {
+        'height': 170,
+        'age': 56,
+        'sex': 'm',
+        'weight': 70
+    }
+
     def getId(self):
         return self.hos_num
 
     def setHosnum(self, hos_num):
         self.hos_num = hos_num
 
-    def getObservation(self, name):
-        if name in self.obs:
-            return self.obs[name]
+    def getObservation(self, name, **kwargs):
         try:
             method = 'get' + name
-            return getattr(self, method)()
+            return getattr(self, method)(**kwargs)
         except AttributeError:
             pass
 
+        if name in self.obs:
+            return self.obs[name]
+
         return None
+
+    def getHeight(self, units='m'):
+        if (units == 'm'):
+            return self.getObservation('height')
+        elif (units == 'cm'):
+            return self.getObservation('height') * 100
+        elif (units == 'inches'):
+            return (self.getObservation('height') * 100) / 2.54
+        raise Exception('invalid units: ' + units)
 
     def __str__(self):
         return self.hos_num
@@ -42,6 +59,7 @@ class OpenEyesPatient(Patient):
         self.setHosnum(hos_num)
 
         self.parseDefinition(definition)
+        
 
     def _getIdentifierFromDefinition(self, definition, label="Hospital Number"):
         for id in definition['identifier']:
